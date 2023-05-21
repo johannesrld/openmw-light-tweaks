a5f49bbebc185d9577834cf93d216639acfd6d6e
tauin <71744831+tauin@users.noreply.github.com>
2023-05-21 22:58:17 +0800

    add project and source code

---
+++ tlconfig.lua
@@ -0,0 +1,11 @@
+-- Copyright (c) 2023 Johannes Richard Levi Dickenson
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+return {
+   build_dir = "scripts/tau",
+   source_dir = "src",
+   include_dir = {"C:/Users/Johannes/Documents/artifacts/teal_declarations/"},
+}
